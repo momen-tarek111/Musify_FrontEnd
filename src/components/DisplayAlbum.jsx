010f1968ed2102ceb0656ccd05773078959d415f
momen-tarek111 <momen.tarek.nagaty@gmail.com>
2026-08-23 02:33:58 +0300

    Update Response Design 9

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -23,11 +23,11 @@ function DisplayAlbum({album}) {
 
       {/* Scrollable wrapper - only scrolls if content doesn't fit */}
       <div className="overflow-x-auto mt-10">
-        <div className="min-w-full w-full grid grid-cols-[minmax(200px,max-content)_140px_50px] sm:grid-cols-[minmax(240px,max-content)_140px_130px_50px] lg:grid-cols-[1fr_180px_150px_60px] gap-2 mb-4 pl-2 text-[#a7a7a7] whitespace-nowrap">
+        <div className="min-w-full w-full grid grid-cols-[minmax(150px,max-content)_80px_36px] sm:grid-cols-[minmax(240px,max-content)_140px_130px_50px] lg:grid-cols-[1fr_180px_150px_60px] gap-2 mb-4 pl-2 text-[#a7a7a7] whitespace-nowrap">
           <p>
             <b className="mr-4">#</b>
           </p>
-          <p>Album</p>
+          <p className="truncate">Album</p>
           <p className="hidden sm:block">Date Added</p>
           <Clock className="block mx-auto w-4" />
         </div>
@@ -37,14 +37,14 @@ function DisplayAlbum({album}) {
           .map((item,index)=>(
             <div
               onClick={()=>playWithId(item._id)}
-              className="min-w-full w-full grid grid-cols-[minmax(200px,max-content)_140px_50px] sm:grid-cols-[minmax(240px,max-content)_140px_130px_50px] lg:grid-cols-[1fr_180px_150px_60px] gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] odd:bg-[#ffffff08] cursor-pointer whitespace-nowrap"
+              className="min-w-full w-full grid grid-cols-[minmax(150px,max-content)_80px_36px] sm:grid-cols-[minmax(240px,max-content)_140px_130px_50px] lg:grid-cols-[1fr_180px_150px_60px] gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] odd:bg-[#ffffff08] cursor-pointer whitespace-nowrap"
               key={index}>
                 <p className="text-white flex items-center">
                   <b className="mr-4 text-[#a7a7a7]">{index + 1}</b>
                   <img src={item.imageUrl} alt="" className="inline w-10 mr-5 min-h-[40px]" />
                   <span>{item.name}</span>
                 </p>
-                <p className="text-[15px]">{album?.name}</p>
+                <p className="text-[15px] truncate">{album?.name}</p>
                 <p className="text-[15px] hidden sm:block">5 days ago</p>
                 <p className="text-[15px] text-center">{item.duration}</p>
             </div>
